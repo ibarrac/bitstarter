665a720f88d1364ab8fbe3fe1ca1d175dca24469
Carlos Ibarra <ibarrac@yahoo.com>
2013-07-17 05:00:22 +0000

    Added url capability to grader.js

--- grader.js
+++ grader.js
@@ -24,6 +24,9 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var util = require('util');
+var rest = require("restler");
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,16 +39,30 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
+var retrieveFile = function(htmlfile) {
+    return fs.readFileSync(htmlfile);
+}
+
+var retrieveURL = function(url) {
+    var text;
+    var receive = function(result,response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            text = result;
+        }
+    }
+
+    rest.get(url).on('complete', receive);
+    return text;
+}
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlString = function(htmlString, checksfile) {
+    $ = cheerio.load(htmlString);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -61,12 +78,24 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>','URL')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var textToCheck;
+    if (program.url)
+    {
+       textToCheck = retrieveURL(program.url);
+    }
+    else
+    {
+       textToCheck = retrieveFile(program.file);
+    }
+
+    var checkJson = checkHtmlString(textToCheck, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
